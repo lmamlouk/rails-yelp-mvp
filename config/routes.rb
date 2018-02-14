73b3836448705c74173d85c74cf0f8939975c003
Lamia Mamlouk <lamia.mamlouk@gmail.com>
2018-02-14 12:48:43 +0100

    exericie 1 - part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "application_controller#action"
+
+  resources :restaurants do
+      resources :reviews, only: [ :index, :new, :create ]
+  end
+    resources :reviews, only: [ :show, :edit, :update, :destroy ]
 end
